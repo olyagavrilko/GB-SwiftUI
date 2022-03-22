075544da42614d6556162bd00ecd02ef6785c2bf
Olya Gavrilko <olyaganeva@gmail.com>
2022-03-22 20:22:45 +0300

    LoginScreen

--- GBSwiftUI/ContentView.swift
+++ GBSwiftUI/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var login = ""
+    @State private var password = ""
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Image("VKLogo")
+                .resizable()
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                .padding([.top, .bottom], 70)
+            TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: $login)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            SecureField("Password", text: $password)
+                .padding(.bottom, 10)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Button(action: logInButtonDidTap, label: {
+                Spacer()
+                Text("Войти")
+                    .fontWeight(.semibold)
+                Spacer()
+            })
+            .padding(.all, 12)
+            .foregroundColor(.white)
+            .background(Color.blue)
+            .cornerRadius(10)
+            .disabled(login.isEmpty || password.isEmpty)
+            Spacer()
+        }
+        .padding()
+    }
+
+    private func logInButtonDidTap() {
+        print("Hello")
     }
 }
 
