30c019faa97a5ccbf3cbf51aa877a6395e9a31f6
Olya Gavrilko <olyaganeva@gmail.com>
2022-04-14 19:59:28 +0300

    Added animations

--- GBSwiftUI/FriendGallery/View/Rows/GalleryRow.swift
+++ GBSwiftUI/FriendGallery/View/Rows/GalleryRow.swift
@@ -12,15 +12,37 @@ struct GalleryRow: View {
     
     let photoURL: URL
     
+    @State private var isLiked = false
+    
     var body: some View {
         
         let width = (UIScreen.main.bounds.size.width - 3) / 2
-        
-        KFImage(photoURL)
-            .cancelOnDisappear(true)
-            .resizable()
-            .frame(width: width, height: width)
-            .aspectRatio(contentMode: .fill)
+        ZStack(alignment: .bottomTrailing) {
+            KFImage(photoURL)
+                .cancelOnDisappear(true)
+                .resizable()
+                .frame(width: width, height: width)
+                .aspectRatio(contentMode: .fit)
+            Image(systemName: isLiked ? "heart.fill" : "heart")
+                .resizable()
+                .frame(width: 30, height: 30)
+                .foregroundColor(isLiked ? .red : .gray)
+                .padding([.trailing, .bottom], 8)
+                .rotationEffect(isLiked ? Angle.degrees(360) : Angle(degrees: 0))
+                .gesture(
+                    TapGesture()
+                        .onEnded { _ in
+                            
+                            if !isLiked {
+                                withAnimation(.interpolatingSpring(stiffness: 350, damping: 5)) {
+                                    isLiked.toggle()
+                                }
+                            } else {
+                                isLiked.toggle()
+                            }
+                        }
+                )
+        }
     }
 }
 
--- GBSwiftUI/Views/CircleImage.swift
+++ GBSwiftUI/Views/CircleImage.swift
@@ -9,6 +9,9 @@ import SwiftUI
 import Kingfisher
 
 struct CircleImage: View {
+    
+    @State private var isScaled = false
+    @State private var isSpring = false
 
     let imageURL: URL
 
@@ -20,5 +23,15 @@ struct CircleImage: View {
             .frame(width: 50, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             .clipShape(Circle())
             .shadow(radius: 7)
+            .scaleEffect(isScaled ? 0.25 : 1)
+            .onTapGesture {
+                withAnimation(.easeInOut(duration: 0.25)) {
+                    isScaled.toggle()
+                    withAnimation(.interpolatingSpring(mass: 0.5, stiffness: 250, damping: 5, initialVelocity: 1)) {
+                        isScaled.toggle()
+                        isSpring.toggle()
+                    }
+                }
+            }
     }
 }
